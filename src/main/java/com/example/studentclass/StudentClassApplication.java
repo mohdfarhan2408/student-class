60a05b50964ff3ff777ac96822920fd242cdf3d2
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-08-26 16:19:46 +0800

    Initiate commit

---
+++ src/main/java/com/example/studentclass/StudentClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentClassApplication.class, args);
+	}
+
+}

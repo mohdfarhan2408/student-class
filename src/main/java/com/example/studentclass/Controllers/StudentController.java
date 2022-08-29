bf1c355f0fd9ff04780324ff661ab7dc285f0840
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-08-29 20:02:12 +0800

    Creating service and controller layers for Student

--- src/main/java/com/example/studentclass/Controllers/StudentController.java
+++ src/main/java/com/example/studentclass/Controllers/StudentController.java
@@ -1,17 +1,43 @@
 package com.example.studentclass.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.studentclass.Models.Status;
+import com.example.studentclass.Models.Student;
+import com.example.studentclass.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/students")
 public class StudentController {
 
-    //Get list of all of active students
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     //GET student By Id
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Student> getById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(this.studentService.getStudentById(id), HttpStatus.OK);
+    }
 
+
+    //Get list of all active students
+    @GetMapping
+    public ResponseEntity<Student> getAllActiveStudents(@RequestParam(name = "status", required = false)Status status) {
+        return new ResponseEntity<>(this.studentService.getAllActiveStudents(status), HttpStatus.OK );
+    }
+
+    
     //POST Student
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student newStudent){
+        return new ResponseEntity<>(this.studentService.createNewStudent(newStudent), HttpStatus.CREATED );
+    }
 
 
 }
--- src/main/java/com/example/studentclass/Services/StudentService.java
+++ src/main/java/com/example/studentclass/Services/StudentService.java
@@ -1,23 +1,39 @@
 package com.example.studentclass.Services;
 
+import com.example.studentclass.Models.Status;
+import com.example.studentclass.Models.Student;
 import com.example.studentclass.Repositories.StudentRepo;
+import org.hibernate.type.descriptor.sql.JdbcTypeFamilyInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
-//    private final StudentRepo studentRepo;
+    private final StudentRepo studentRepo;
 
-//    @Autowired
-//    public StudentService(StudentRepo studentRepo) {
-//        this.studentRepo = studentRepo;
-//    }
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
 
 
-    //Get list of all of active students
+    public Student getStudentById(Long id) {
+        return studentRepo.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Student wit id:" + id + "doesn't exist"));
+    }
 
-    //GET student By Id
 
-    //POST Student
+    public Student getAllActiveStudents(Status status) {
+        return null;
+
+        /*
+        * Check the table where student_status = Active.
+        *
+        *  */
+    }
+
+    public Student createNewStudent(Student newStudent) {
+        return studentRepo.save(newStudent);
+    }
 }

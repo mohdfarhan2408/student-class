2361ecb444c691c18559ec2f49f58d417cef1c8c
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-09-05 16:40:10 +0800

    Edit custom exception

--- src/main/java/com/example/studentclass/Exceptions/ClassNotFoundException.java
+++ src/main/java/com/example/studentclass/Exceptions/ClassNotFoundException.java
@@ -2,15 +2,17 @@ package com.example.studentclass.Exceptions;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseBody
 public class ClassNotFoundException extends RuntimeException{
      private String fieldName;
-     private Object fieldValue;
+     private Long fieldValue;
 
-    public ClassNotFoundException(String fieldName, Object fieldValue) {
+    public ClassNotFoundException(String fieldName, Long fieldValue) {
         super(String.format("Class Not Found With %s: %s", fieldName, fieldValue));
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;

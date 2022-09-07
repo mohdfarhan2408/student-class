3719edbbd9aed5124f45a4cede99ab378d32f4ec
codex95 <afiqhamdan95@gmail.com>
2022-09-07 10:08:32 +0800

    Add unit test for create new student

--- src/test/java/com/example/studentclass/Services/ClassServiceTest.java
+++ src/test/java/com/example/studentclass/Services/ClassServiceTest.java
@@ -1,12 +1,16 @@
 package com.example.studentclass.Services;
 
+import com.example.studentclass.Models.Class;
+import com.example.studentclass.Models.Status;
 import com.example.studentclass.Repositories.ClassRepo;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -17,29 +21,44 @@ class ClassServiceTest {
     @Mock
     private ClassRepo classRepo;
     private ClassService underTest;
-
-    @Test
-    @Disabled
-    void getClassById() {
-    }
+//    private Class newClass;
+//    private Long id;
 
     @BeforeEach
     void setUp() {
         underTest = new ClassService(classRepo);
     }
-
+    @Test
+    @Disabled
+    void getClassById() {
+//        //when
+//        underTest.getClassById(id);
+//        //then
+//        verify(classRepo).findById(id);
+    }
     @Test
     void getAllActiveClasses() {
+        //when
         underTest.getAllActiveClasses();
+        //then
         verify(classRepo).findAllActiveClasses();
     }
     @Test
-    @Disabled
     void createNewClass() {
+        //given
+        Class newClass = new Class(
+                "Bestari",
+                Status.ACTIVE
+        );
+        //when
+        underTest.createNewClass(newClass);
+        //then
+        verify(classRepo).save(newClass);
     }
 
     @Test
     @Disabled
     void updateClassDetails() {
     }
+
 }

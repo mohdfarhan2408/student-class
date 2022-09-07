820dae2ca8d9a52cb8bb3d2baabf844c98ad5a33
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-09-07 10:35:36 +0800

    Adding basic swagger openApi UI

--- src/main/java/com/example/studentclass/Controllers/ClassController.java
+++ src/main/java/com/example/studentclass/Controllers/ClassController.java
@@ -2,6 +2,8 @@ package com.example.studentclass.Controllers;
 
 import com.example.studentclass.Models.Class;
 import com.example.studentclass.Services.ClassService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/class")
+@Tag(name = "Class", description = "CRUD for Class domain object")
 public class ClassController {
 
     private final ClassService classService;
@@ -22,18 +25,21 @@ public class ClassController {
 
     //Get class by Id;
     @GetMapping(path = "/{id}")
+    @Operation(summary = "get Class By Id", description = "return a class based on id", tags = {"Class"})
     public ResponseEntity<Class> getById(@PathVariable("id") Long id){
         return new ResponseEntity<>(this.classService.getClassById(id), HttpStatus.OK);
     }
 
     //Get all active classes;
     @GetMapping(path = "/active")
+    @Operation(summary = "get all active Classes", description = "return a list of active classes", tags = {"Class"})
     public ResponseEntity<List<Class>> getActiveClasses() {
         return new ResponseEntity<>(this.classService.getAllActiveClasses(), HttpStatus.OK );
     }
 
     //POST a class;
     @PostMapping(path = "/create")
+    @Operation(summary = "Post a class", description = "Create a class", tags = {"Class"})
     public ResponseEntity<Class> createClass(@RequestBody Class newClass){
         return new ResponseEntity<>(this.classService.createNewClass(newClass), HttpStatus.CREATED );
     }
--- src/main/java/com/example/studentclass/Controllers/StudentController.java
+++ src/main/java/com/example/studentclass/Controllers/StudentController.java
@@ -5,6 +5,8 @@ import com.example.studentclass.Models.Student;
 import com.example.studentclass.Repositories.ClassRepo;
 import com.example.studentclass.Repositories.StudentRepo;
 import com.example.studentclass.Services.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/student")
+@Tag(name = "Student", description = "CRUD for Student domain object")
 public class StudentController {
 
     private final StudentService studentService;
@@ -28,6 +31,7 @@ public class StudentController {
 
     //GET student By Id
     @GetMapping(path = "/{id}")
+    @Operation(summary = "get student by id", description = "return a student by it's id", tags = {"Student"})
     public ResponseEntity<Student> getById(@PathVariable("id") Long id){
         return new ResponseEntity<>(this.studentService.getStudentById(id), HttpStatus.OK);
     }
@@ -35,15 +39,16 @@ public class StudentController {
 
     //Get list of all active students
     @GetMapping(path = "/active")
+    @Operation(summary = "get all active Students", description = "return a list of active students", tags = {"Student"})
     public ResponseEntity<List<Student>> getAllActiveStudents() {
         return new ResponseEntity<>(this.studentService.getActiveStudents(), HttpStatus.OK );
     }
 
     //POST Student
     @PostMapping(path = "/create/{classId}")
+    @Operation(summary = "Post a student", description = "create a new student and assign it to respective class", tags = {"Student"})
     public ResponseEntity<Student> createStudent(@RequestBody Student newStudent, @PathVariable("classId") Long id){
         return new ResponseEntity<>(this.studentService.createNewStudent(newStudent,id), HttpStatus.CREATED);
     }
-    
 
 }

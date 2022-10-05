d4dc2b3668c0401c1feaf063d7849d1d068e7870
Mohd Farhan <farhanabdulrahman98@gmail.com>
2022-10-05 14:27:29 +0800

    adding notes

--- src/main/java/com/example/studentclass/StudentClassApplication.java
+++ src/main/java/com/example/studentclass/StudentClassApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentClassApplication {
 
+	//Note
 	public static void main(String[] args) {
 		SpringApplication.run(StudentClassApplication.class, args);
 	}
